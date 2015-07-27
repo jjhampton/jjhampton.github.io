58e644bb9f3e8325d0ec949069aa27535d9291bb
John Hampton <john_j_hampton@yahoo.com>
2015-07-27 01:27:58 -0400

    Added correctly sized screen captures for portfolio pieces, text below them, and buttons linking to their sites

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@
 	$(window).load(function(){
       $("#navigation").sticky({ topSpacing: 0 });
     });
-	
+
 
 	wow = new WOW(
       {
@@ -19,9 +19,9 @@
 	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 	}, function() {
 	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-	});	
+	});
 
-	
+
 	//jQuery to collapse the navbar on scroll
 	$(window).scroll(function() {
 		if ($(".navbar").offset().top > 50) {
@@ -30,17 +30,17 @@
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
 		}
 	});
-	
+
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$('.navbar-nav li a').bind('click', function(event) {
 			var $anchor = $(this);
 			var nav = $($anchor.attr('href'));
 			if (nav.length) {
-			$('html, body').stop().animate({				
-				scrollTop: $($anchor.attr('href')).offset().top				
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
 			}, 1500, 'easeInOutExpo');
-			
+
 			event.preventDefault();
 			}
 		});
@@ -53,14 +53,14 @@
 		});
 	});
 
-	
+
 	$('.testimonialslide').flexslider({
 	animation: "slide",
 	slideshow: false,
 	directionNav:false,
 	controlNav: true
 	});
-	
+
 	//owl carousel
 	$('#owl-works').owlCarousel({
             items : 4,
@@ -70,9 +70,9 @@
             itemsTabletSmall: [550,2],
             itemsMobile : [480,2],
         });
-	
+
 	//nivo lightbox
-	$('.owl-carousel .item a').nivoLightbox({
+	$('.owl-carousel .works-lightbox').nivoLightbox({
 		effect: 'fadeScale',                             // The effect to use when showing the lightbox
 		theme: 'default',                           // The lightbox theme to use
 		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
@@ -86,13 +86,13 @@
 		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
 		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
 	});
-	
+
 
 	jQuery('.appear').appear();
 	jQuery(".appear").on("appear", function(data) {
 			var id = $(this).attr("id");
 			jQuery('.nav li').removeClass('active');
-			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");					
+			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");
 		});
-	
+
 })(jQuery);
