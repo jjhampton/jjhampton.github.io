b5e88a023f3cb758215fc731e3935cef43a06f02
John Hampton <john_j_hampton@yahoo.com>
2018-03-11 02:55:14 -0400

    Add Material fixed waterfall toolbar & typography

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import {MDCRipple} from '@material/ripple';
+import {MDCToolbar} from '@material/toolbar';
 
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+const toolbar = MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
+toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
