56777ad5cc09d9d0eb21d9c15e81e9872d5090fa
John Hampton <john_j_hampton@yahoo.com>
2018-03-11 14:50:33 -0400

    Remove Material Design Components & Webpack

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-import {MDCRipple} from '@material/ripple';
-import {MDCToolbar} from '@material/toolbar';
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
-
-const toolbar = MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
-toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
