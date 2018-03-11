58f223bc1302345199895fb2f6b3efea524a5601
John Hampton <john_j_hampton@yahoo.com>
2018-03-11 17:53:03 -0400

    Add JS & CSS transitions for sticky header

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+/* sticky-header listener */
+/* source: https://remysharp.com/2017/06/29/smooth-scroll-with-sticky-nav */
 
+var $stickyHeader = document.getElementById('sticky-header');
+var boundary = $stickyHeader.offsetHeight;
+
+window.onscroll = function (event) {
+  requestAnimationFrame(checkSticky);
+};
+
+function checkSticky() {
+  var y = window.scrollY + 2;
+
+  var isSticky = document.body.classList.contains('sticky-parent');
+  if (y > boundary) {
+    if (!isSticky)
+      document.body.classList.add('sticky-parent');
+  }
+  else if (isSticky)
+    document.body.classList.remove('sticky-parent');
+}
